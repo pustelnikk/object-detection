21ff7926dad26831efafe711a610231048516bd7
s22674 <s22674@pjwstk.edu.pl>
2023-03-25 21:55:48 +0100

    init

---
+++ main.py
@@ -0,0 +1,89 @@
+import cv2 as cv
+import numpy as np
+
+'''
+Video proccessing
+Colorspaces 
+Morphological operations (opening, closing)
+Srodek ciezkości piłki (suma współrzedcnych lub contours featuers)
+ramka
+'''
+
+def main():
+    video()
+    #rgb_to_hsv()
+
+
+
+
+def video():
+    
+    cap = cv.VideoCapture('movingball.mp4')
+
+    while cap.isOpened():
+
+        ret, frame = cap.read()
+        # if frame is read correctly ret is True
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+            break
+        
+
+      
+
+        hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+
+        lower_red = np.array([0,125,50])
+        upper_red = np.array([15, 220,255])
+
+        lower_rose = np.array([330, 100,100])
+        upper_rose = np.array([350,255,255])
+        
+        
+        lower_rose_2 = np.array([165,100,200])
+        upper_rose_2 = np.array([185, 255,255])
+        
+        lower_rose_3 = np.array([165,80,170])
+        upper_rose_3= np.array([185, 255,255])
+        
+        lower_orange = np.array([35,150,150])
+        upper_orange = np.array([40, 255,255])
+        
+        lower_brown = np.array([16,200,200])
+        upper_brown = np.array([25,250,250])
+
+        mask_red = cv.inRange(hsv,lower_red,upper_red)
+        mask_rose = cv.inRange(hsv,lower_rose,upper_rose)
+        mask_rose_2 = cv.inRange(hsv,lower_rose_2,upper_rose_2)
+        mask_rose_3 = cv.inRange(hsv,lower_rose_3,upper_rose_3)
+        mask_orange = cv.inRange(hsv,lower_orange,upper_orange)
+
+        mask_brown = cv.inRange(hsv,lower_brown,upper_brown)
+
+
+        mask = mask_red | mask_rose | mask_rose_2 |  mask_rose_3 | mask_orange
+        
+        kernel_closing = np.ones((2,2), np.uint8)
+        closing = cv.morphologyEx(mask, cv.MORPH_CLOSE, kernel_closing)
+        #mask = np.subtract(mask_brown, mask)
+        res = cv.bitwise_and(frame,frame,mask=closing)
+
+
+
+        cv.imshow('frame', frame)
+        cv.imshow('mask', closing)
+        cv.imshow('res', res)
+
+
+        if cv.waitKey(1) == ord('q'):
+            break
+    
+    cap.release()
+    cv.destroyAllWindows()
+
+def rgb_to_hsv():
+    green = np.uint8([[[86,118,145 ]]])
+    hsv_green = cv.cvtColor(green,cv.COLOR_BGR2HSV)
+    print( hsv_green )
+if __name__ == '__main__':
+    main()
